c1e9b8106021376b59472c8d51938d8bc5222b88
Luca Sforza <lucasforza1234@icloud.com>
2026-02-09 18:37:54 +0100

    uv is the best

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from net.base import Net
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
 import wandb
+import torch
 from lightning.pytorch.loggers import WandbLogger
 from dataset.loader import load_mnist
 
@@ -14,51 +15,57 @@ from omegaconf import OmegaConf
 
 import os
 
-@hydra.main(config_path="../cfg", config_name="base")
+
+@hydra.main(
+    config_path="/home/softdream/Programming/gits/MNIST-FDS/cfg",
+    config_name="base",
+    version_base="1.3",
+)
 def main(cfg: DictConfig):
     lit.seed_everything(cfg.seed)
     wandb_logger = WandbLogger(**cfg.wandb)
 
     model = Net(cfg.net)
-
     train_loader, val_loader, test_loader = load_mnist(**cfg.dataset)
+    trainer = lit.Trainer(
+        logger=wandb_logger,
+        callbacks=[
+            cb.EarlyStopping(
+                monitor="val_acc", patience=3, verbose=True, mode="max", min_delta=1e-2
+            )
+        ],
+        **cfg.trainer,
+    )
 
-    trainer = lit.Trainer(logger=wandb_logger, callbacks=[
-        cb.EarlyStopping(
-            monitor="val_acc",
-            patience=3,
-            verbose=True,
-            mode="max", min_delta=1e-2
-        )], **cfg.trainer)
-
-    print('Training...')
+    print("Training...")
     trainer.fit(model, train_loader, val_loader)
-    
-    print('Testing...')
+
+    print("Testing...")
     trainer.test(model, test_loader)
-    
+
     hyperparams_dict = OmegaConf.to_container(cfg, resolve=True)
     hyperparams_dict["info"] = {  # type: ignore
         "num_params": get_num_params(model),
     }
+
+    torch.save(model, "weights.pt")
     wandb_logger.log_hyperparams(hyperparams_dict)  # type: ignore
+
 
 def get_num_params(module):
     """
     Returns the number of parameters in a Lightning module.
-    
+
     Args:
         module (lightning.pytorch.LightningModule): The Lightning module to get the number of parameters for.
-    
+
     Returns:
         int: The number of parameters in the module.
     """
-    total_params = sum(p.numel() for p in module.parameters() )
+    total_params = sum(p.numel() for p in module.parameters())
     return total_params
 
 
-
-
 if __name__ == "__main__":
-    os.environ['HYDRA_FULL_ERROR'] = "1"
+    os.environ["HYDRA_FULL_ERROR"] = "1"
     main()
